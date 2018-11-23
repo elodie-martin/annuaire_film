3abb17804ce26010d4727024c6cb35f6ae64babf
Lucas Robin <lucas.rob1@live.fr>
2018-11-23 20:59:35 +0100

    Affichage des informations d'un  films via son  noms terminé. Préparation de la partial _card.

--- controllers/film_controller.php
+++ controllers/film_controller.php
@@ -3,32 +3,60 @@
 require_once('vendor/autoload.php');
 require_once('models/film_model.php');
 
-$loader = new Twig_Loader_Filesystem('views');
-$twig = new Twig_Environment($loader);
-
-// $list = getList('SELECT ID, Titre FROM Film');
-
-// echo $list;
-
-// echo $twig->render('home_view.twig', array('message' => "Hello World !", 'list' => $list ));
-
 if (isset($_SERVER["REQUEST_URI"])) {
 	// $action = (count($requete) < 3)? "liste": $requete[2];
 	// $id = (count($requete) < 4)? 0 : intval($requete[3]);
 	$requete = explode("/", trim($_SERVER['REQUEST_URI'], "/"));
 	$action = (count($requete) < 3)? NULL: $requete[2];
+}
 
+function renderFilm($Id) {
+	$loader = new Twig_Loader_Filesystem('views');
+	$twig = new Twig_Environment($loader);
+	$film = getFilm($Id); // Fonction importé depuis film_model.php
+	// print_r($film);
+	echo $twig->render('film_view.twig', array('film' => $film[0]));
 }
 
 switch ($action) {
 	case '1':
-		$films = getFilm('1');
-		echo $twig->render('film_view.twig', array('films' => $films));
+		renderFilm('1');
 		break;
-
-	
+	case '2':
+		renderFilm('2');
+		break;
+	case '3':
+		renderFilm('3');
+		break;
+	case '4':
+		renderFilm('4');
+		break;
+	case '5':
+		renderFilm('5');
+		break;
+	case '6':
+		renderFilm('6');
+		break;
+	case '7':
+		renderFilm('7');
+		break;
+	case '8':
+		renderFilm('8');
+		break;
+	case '9':
+		renderFilm('9');
+		break;								
+	case '10':
+		renderFilm('10');
+		break;	
+	case '11':
+		renderFilm('11');
+		break;	
+	case '12':
+		renderFilm('12');
+		break;			
 	default:
-		# code...
+		# code... ouais.. encore...
 		break;
 }
 
--- models/film_model.php
+++ models/film_model.php
@@ -8,16 +8,16 @@ function getFilm($idFilm) {
 	global $bdd;
 
 	// ICI METTRE LA REQUETE
-	$sql = "SELECT Film.Titre,Film.Sortie,Film.Description,Realisateur.Nom,Realisateur.Prenom,Genre.Themes 
+	$sql = "SELECT Film.Titre,Film.Sortie,Film.Description,Realisateur.Nom,Realisateur.Prenom, GROUP_CONCAT(Genre.Themes) AS Genres
 			FROM Film 
-			INNER JOIN Liaison_ID_Genre_Film on Liaison_ID_Genre_Film.ID_Film = Film.ID 
-			INNER JOIN Genre on Genre.ID = Liaison_ID_Genre_Film.ID_Genre
-			INNER JOIN Table_Liaison_ID_Film_Realisateur on Table_Liaison_ID_Film_Realisateur.ID_Film = Film.ID 
-			INNER JOIN Realisateur on Realisateur.ID = Table_Liaison_ID_Film_Realisateur.ID_Realisateur
-			WHERE ID = { $idFilm }";
-
+			INNER JOIN Liaison_ID_Genre_Film ON Liaison_ID_Genre_Film.ID_Film = Film.ID 
+			INNER JOIN Genre ON Genre.ID = Liaison_ID_Genre_Film.ID_Genre
+			INNER JOIN Table_Liaison_ID_Film_Realisateur ON Table_Liaison_ID_Film_Realisateur.ID_Film = Film.ID 
+			INNER JOIN Realisateur ON Realisateur.ID = Table_Liaison_ID_Film_Realisateur.ID_Realisateur
+			WHERE Film.ID = :idFilm";
 
 	$response = $bdd->prepare( $sql );
+	$response->bindParam(':idFilm', $idFilm, PDO::PARAM_STR);
     $response->execute();
     $film = $response->fetchAll(PDO::FETCH_ASSOC);
 
