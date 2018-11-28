502844383a2aced13ba616ce32237b91a61ef0d8
Elodie Martin <elodie.m@codeur.online>
2018-11-28 09:25:04 +0100

    adaptation affichage par annees

---
+++ controllers/years_controller.php
@@ -0,0 +1,25 @@
+<?php 
+
+require_once('vendor/autoload.php');
+require_once('models/years_model.php');
+
+function renderFilmsByYears($years) {
+	$loader = new Twig_Loader_Filesystem('views');
+	$twig = new Twig_Environment($loader);
+	$filmsByYears = getFilmsByYears($years); // Fonction importé depuis film_model.php
+	// $gendeForEachFilms = getYearsOfFilm($filmsByYears.ID) 
+	//print_r($filmsByYears); // DEBUG HERE
+	echo $twig->render('years_view.twig', array('film' => $filmsByYears));
+}
+
+if (isset($_SERVER["REQUEST_URI"])) {
+	// $action = (count($requete) < 3)? "liste": $requete[2];
+	// echo $_SERVER["REQUEST_URI"];
+	$requete = explode("/", trim($_SERVER['REQUEST_URI'], "/"));
+	$id = (count($requete) < 4)? 0 : intval($requete[3]);
+	// $action = (count($requete) < 3)? NULL: $requete[2];
+
+	renderFilmsByYears($id);
+}
+
+?>
--- index.php
+++ index.php
@@ -17,7 +17,10 @@
 				break;
 			case 'gender':
 				require_once("controllers/gender_controller.php");
-				break;		
+				break;
+			case 'years':
+				require_once("controllers/years_controller.php");
+				break;	
 			default:
 				require_once("controllers/404.php");
 				break;
