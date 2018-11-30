fcb3edb1c75bd669bfd2db127af5da4709a5baf9
vagrant <vagrant@vagrant.vm>
2018-11-30 21:04:56 +0000

    FIX bdd

--- models/connect_bdd.php
+++ models/connect_bdd.php
@@ -3,7 +3,7 @@
     // Fichier pour se connecter à la base de donnée.d
     $baseurl = "http://localhost/annuaire_film/";
 	$username = 'root';
-	$password = '';
+	$password = 'online@2017';
 	$database ='projet_annuaire_films';
   
 	$host = 'localhost';
